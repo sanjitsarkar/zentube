7ce2fe588ab04d2086b3c9a86e50766d512f7844
Sanjit Sarkar <ssanjit856@gmail.com>
2022-04-08 00:12:36 +0530

    Update index.jsx

--- src/pages/SingleVideoPage/index.jsx
+++ src/pages/SingleVideoPage/index.jsx
@@ -2,6 +2,26 @@ import React, { useEffect } from "react";
 import Loader from "../../components/Loader";
 import VideoCard from "../../components/VideoCard.jsx";
 import { useVideos } from "../../context/VideosContext";
+<<<<<<< HEAD
+import RelatedVideos from "./RelatedVideos";
+import VideoInfo from "./VideoInfo";
+import "./SingleVideoPage.css";
+import { useNav } from "../../context/NavContext";
+import NotAvailable from "../../components/NotAvailable";
+const SingleVideoPage = () => {
+  const location = useLocation();
+  const { setActiveItem } = useNav();
+  useEffect(() => {
+    setActiveItem("");
+  }, []);
+  const { fetchVideoInfo, videoInfo } = useVideos();
+  useEffect(() => {
+    let pathName = location.pathname.split("/");
+    let videoId = pathName[pathName.length - 1];
+    fetchVideoInfo(videoId);
+  }, [location]);
+=======
+>>>>>>> db8e5dcdd74776570063d97caee3d94d4811bbdf
 
 const RelatedVideos = ({ category, videoId }) => {
   const { filterVideos, relatedVideos } = useVideos();
@@ -9,18 +29,31 @@ const RelatedVideos = ({ category, videoId }) => {
     await filterVideos({ category, type: "related" });
   }, []);
   return (
-    <div className="mt-3">
-      <h3>Related Videos</h3>
-      <div className={`btn font-bold  btn-primary tag mt-2`}>{category}</div>
+    <Layout>
+      <div className="row flex-wrap ">
+        {videoInfo.loading && (
+          <div className="w-screen h-72 grid place-content-center place-items-center gap-1">
+            <Loader />
+          </div>
+        )}
+        {!videoInfo.loading && videoInfo.data.length !== 0 && (
+          <>
+            <VideoInfo video={videoInfo.data} />
+            <RelatedVideos
+              videoId={videoInfo.data._id}
+              category={videoInfo.data.category}
+            />
+          </>
+        )}
 
-      {relatedVideos.loading && <Loader />}
-      <div className=" mt-3 gap-05 video-grid  ">
-        {!relatedVideos.loading &&
-          relatedVideos.data.length > 0 &&
-          relatedVideos.data.map((video, i) => {
-            if (video._id !== videoId)
-              return <VideoCard video={video} key={video._id} />;
-          })}
+        {!videoInfo.loading && videoInfo.data.length === 0 && (
+          <div className="w-screen h-4-6 grid place-content-center place-items-center gap-1">
+            <NotAvailable title="Video is not available" />
+            <Link to="/" className="btn btn-primary w-fit">
+              Go to Home
+            </Link>
+          </div>
+        )}
       </div>
     </div>
   );
