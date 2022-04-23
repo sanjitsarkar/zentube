30fef1744617f66129c2bb74a81162dcdc692015
Sanjit <ssanjit856@gmail.com>
2022-04-23 23:20:23 +0530

    fixed thumbnail

--- src/backend/db/videos.js
+++ src/backend/db/videos.js
@@ -14,8 +14,7 @@ export const videos = [
     creatorImage:
       "https://i.pinimg.com/originals/52/16/a0/5216a0259d75cd14780873a5384b4d7e.jpg",
     creatorName: "Tomorowland",
-    thumbNail:
-      "https://i.ytimg.com/an_webp/rbMv6o1_3gA/mqdefault_6s.webp?du=3000&sqp=CICpj5MG&rs=AOn4CLAQbNcJwptyZZAiEMUsfK6vetKMmQ",
+    thumbNail: "https://i.postimg.cc/rm0J1Y4H/mqdefault-6s.webp",
     viewCount: 1200345,
     videoDesc: "David Guetta@Tomorrowland, Saturday, July 27th 2019",
     publishedAt: toTimestamp("03/03/2022 23:31:30"),
@@ -45,8 +44,7 @@ export const videos = [
     creatorImage:
       "https://yt3.ggpht.com/ytc/AKedOLTGik__A9u5YdLS2VtAkkzskCV0f-L2cy6XM6Hh=s48-c-k-c0x00ffffff-no-rj",
     creatorName: "Rousseau",
-    thumbNail:
-      "https://i.ytimg.com/an_webp/WJ3-F02-F_Y/mqdefault_6s.webp?du=3000&sqp=CMinj5MG&rs=AOn4CLD_jrxbfZzdvjTQt3cPE7F9fj9oig",
+    thumbNail: "https://i.postimg.cc/dVsjH0dr/mqdefault-6s.webp",
     viewCount: 47538896,
     videoDesc:
       "Hello, I'm Rousseau, I make piano covers of classical and pop songs with a reactive visualizer. New videos every Monday and Thursday!",
@@ -61,8 +59,7 @@ export const videos = [
     creatorImage:
       "https://yt3.ggpht.com/ytc/AKedOLTENAX_RNlwH5L4oz7gt1B7VdX6ZQBRiYUHxCxy=s68-c-k-c0x00ffffff-no-rj",
     creatorName: "Kassia",
-    thumbNail:
-      "https://i.ytimg.com/an_webp/5Yamu70Z_FI/mqdefault_6s.webp?du=3000&sqp=CMybj5MG&rs=AOn4CLDa06MN9WnigedL7BbkI1jd4YplrQ",
+    thumbNail: "https://i.postimg.cc/k5hstd28/mqdefault-6s.webp",
     viewCount: 538896,
     videoDesc: "Mozart – Lacrimosa",
     publishedAt: toTimestamp("03/13/2020 23:31:30"),
@@ -84,8 +81,7 @@ export const videos = [
     creatorImage:
       "https://yt3.ggpht.com/YIBi8NVC87fMfJHfQ2O0dyzjis7tUlO7VqWLhk1lq1fkIOQTrpX_Ip7G6S_u0IJosXYSe_Z9=s88-c-k-c0x00ffffff-no-rj",
     creatorName: "NoCopyrightSounds",
-    thumbNail:
-      "https://i.ytimg.com/an_webp/uDEy060DZXg/mqdefault_6s.webp?du=3000&sqp=CNCij5MG&rs=AOn4CLD1fbc1M72altMJY3SwU6DqI4YXBQ",
+    thumbNail: "https://i.postimg.cc/cHpcXMML/mqdefault-6s.webp",
     viewCount: 601159,
     videoDesc:
       "NCS (NoCopyrightSounds): Empowering Creators through Copyright / Royalty Free Music",
@@ -109,8 +105,7 @@ export const videos = [
     creatorImage:
       "https://yt3.ggpht.com/ANmgv2-dyHCi7tnIuUtJLcKUI9QIk2Rp8Fojl4Gwe4PLxFCxD-S0jvJdO7JvEbHnT3Rk_zaD=s88-c-k-c0x00ffffff-no-rj",
     creatorName: "MrSuicideSheep",
-    thumbNail:
-      "https://i.ytimg.com/an_webp/L7T6UOkHkJo/mqdefault_6s.webp?du=3000&sqp=CKiYj5MG&rs=AOn4CLD4ZsrVky2hUNUA1J5X-XAn5OW3xw",
+    thumbNail: "https://i.postimg.cc/Mp5dqBx9/mqdefault-6s.webp",
     viewCount: 54689437,
     videoDesc: "Silent Child - F**K YOU",
     publishedAt: toTimestamp("03/13/2019 23:31:30"),
@@ -154,8 +149,7 @@ export const videos = [
     creatorImage:
       "https://yt3.ggpht.com/ytc/AKedOLQpH7ilr8k0ALkDT1e8rHpVVlB2EFzB_WhfnjUx=s68-c-k-c0x00ffffff-no-rj",
     creatorName: "Lyrical Lemonade",
-    thumbNail:
-      "https://i.ytimg.com/an_webp/mzB1VGEGcSU/mqdefault_6s.webp?du=3000&sqp=CP-oj5MG&rs=AOn4CLBbTvH_Eu6pL-HJhakfxFzA_2dV1g",
+    thumbNail: "https://i.postimg.cc/NFRkV9p7/mqdefault-6s.webp",
     viewCount: 807538896,
     videoDesc: "Juice WRLD - Lucid Dreams (Directed by Cole Bennett)",
     publishedAt: toTimestamp("09/1/2019 23:31:30"),
@@ -170,8 +164,7 @@ export const videos = [
     creatorImage:
       "https://yt3.ggpht.com/ytc/AKedOLRkrSDf5fh-1FJCxsbFMzkqyiO3Q655hmNiKIcTBg=s88-c-k-c0x00ffffff-no-rj",
     creatorName: "Machine Gun Kelly",
-    thumbNail:
-      "https://i.ytimg.com/an_webp/SQ2HstgvzZ4/mqdefault_6s.webp?du=3000&sqp=CM6Lj5MG&rs=AOn4CLAWgSvnHmqThFLRQVnkaTZIgrb5qQ",
+    thumbNail: "https://i.postimg.cc/1trmFHTY/mqdefault-6s.webp",
     viewCount: 47538896,
     videoDesc:
       "Machine Gun Kelly - Mind of a Stoner ft. Wiz Khalifa (OFFICIAL MUSIC VIDEO)",
@@ -193,8 +186,7 @@ export const videos = [
     creatorImage:
       "https://yt3.ggpht.com/MqKm9xyjonzkICKA78ir0AM-WUR47ntkBeJlgHeIk_rUnPuukiWtzOEmU7UjO8cFoPrBatCh3As=s88-c-k-c0x00ffffff-no-rj",
     creatorName: "Taylor Swift",
-    thumbNail:
-      "https://i.ytimg.com/an_webp/tCXGJQYZ9JA/mqdefault_6s.webp?du=3000&sqp=CNCrj5MG&rs=AOn4CLCqBLuQhMan6SCvQ9L3kRNMeQJo1A",
+    thumbNail: "https://i.postimg.cc/zX1jh7Gd/mqdefault-6s.webp",
     viewCount: 908538896,
     videoDesc: "Taylor Swift - Delicate (Official Video)",
     publishedAt: toTimestamp("03/13/2018 23:31:30"),
@@ -208,8 +200,7 @@ export const videos = [
     creatorImage:
       "https://yt3.ggpht.com/-syoRcPPGt3NE3TzbKJsefa5Rej4w_lSof-aXSm1Q9cDx5Gyn-Cuus8UKjKBtILs_Bqwgc_i0A=s88-c-k-c0x00ffffff-no-rj",
     creatorName: "Ed Sheeran",
-    thumbNail:
-      "https://i.ytimg.com/an_webp/orJSJGHjBLI/mqdefault_6s.webp?du=3000&sqp=CMizj5MG&rs=AOn4CLCE1Ey21gXPWxEniPxs6o0sn8_KgQ",
+    thumbNail: "https://i.postimg.cc/255T65Sr/mqdefault-6s.webp",
     viewCount: 908538896,
     videoDesc: "Ed Sheeran - Bad Habits [Official Video]",
     publishedAt: toTimestamp("01/13/2022 23:31:30"),
